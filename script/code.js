b545df23b2e0b8f44a123800da68a6692eaba400
Asiphe04 <asiphendimlana40@gmail.com>
2023-06-15 00:52:43 +0200

    homedone

---
+++ script/code.js
@@ -0,0 +1,9 @@
+// HOME PAGE
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = "block";
+  }
+  
+  function closePopup(popupId) {
+    document.getElementById(popupId).style.display = "none";
+  }
+  //END OF HOME PAGE
